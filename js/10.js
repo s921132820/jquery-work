c5403552e6af4bbce1e04de51eb0fb3f2b81220a
s921132820 <s921132820@gmail.com>
2025-02-13 10:42:42 +0900

    forLoop

--- js/10.js
+++ js/10.js
@@ -7,4 +7,47 @@ $(document).ready(function() {
     { name : '이서', age : 18 },
     { name : '가을', age : 23 },
   ]
+
+  $("#forLoop").on("click", function() {
+    let result = ''
+    // for 구문
+    for(let i=0; i < data.length; i ++) {
+      result = result + `
+      <tr>
+        <td>${i + 1}</td>
+        <td>${data[i].name}</td>
+        <td>${data[i].age}</td>
+      </tr>
+      `
+    }
+    $("#forOut").html(result)
+  })
+
+  $("#forEach").on("click", function() {
+    let result = ''
+    data.forEach((user, index) => {
+      result = result + `
+            <tr>
+        <td>${index + 1}</td>
+        <td>${user.name}</td>
+        <td>${user.age}</td>
+      </tr>
+      `
+    })
+    $("#forEachTable").html(result)
+  })
+
+  $("#jQueryEach").on("click", function() {
+    let result = ''
+    $.each(data, (index, x) => {
+      result = result + `
+      <tr>
+        <td>${index + 1}</td>
+        <td>${x.name}</td>
+        <td>${x.age}</td>
+      </tr>
+      `
+    })
+    $("#jqueryEachTable").html(result)
+  })
 })
